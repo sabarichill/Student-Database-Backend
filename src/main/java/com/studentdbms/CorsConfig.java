506148183c14040c3217b03cfad32385c638a45a
Sabari S <sabarichill007@gmail.com>
2026-06-27 12:43:44 +0530

    Update CorsConfig.java

--- src/main/java/com/studentdbms/CorsConfig.java
+++ src/main/java/com/studentdbms/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
+                registry.addMapping("/**")
                         .allowedOriginPatterns("*")
                         .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                         .allowedHeaders("*")
